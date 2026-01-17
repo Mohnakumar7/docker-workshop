cb663699b1db5d5e89e6607edd5621ecab341ac7
V Mohankumar <mohankumarv8861@gmail.com>
2026-01-17 13:43:01 +0000

    docker-compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -27,45 +28,60 @@ parse_dates = [
 ]
 
 
-def run():
-    year = 2021
-    month = 1
-    pg_user="root" 
-    pg_pass="root" 
-    pg_db="ny_taxi" 
-    pg_host="localhost"
-    pg_port="5432"
-    chunksize = 100000
-    target_table = "yellow_taxi_data"
-
+def run(
+    year: int = 2021,
+    month: int = 1,
+    pg_user: str = "root",
+    pg_pass: str = "root",
+    pg_db: str = "ny_taxi",
+    pg_host: str = "localhost",
+    pg_port: int = 5432,
+    chunksize: int = 100000,
+    target_table: str = "yellow_taxi_data",
+):
     prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-
 
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunksize
+        chunksize=chunksize,
     )
 
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(n=0).to_sql(
-                name=target_table, 
-                con=engine, 
-                if_exists='replace'
-                )
+            df_chunk.head(n=0).to_sql(name=target_table, con=engine, if_exists="replace")
             first = False
 
-        df_chunk.to_sql(
-            name=target_table, 
-            con=engine, 
-            if_exists='append'
-        )
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
+
+@click.command()
+@click.option("--year", default=2021, type=int, help="Year of the dataset")
+@click.option("--month", default=1, type=int, help="Month of the dataset (1-12)")
+@click.option("--pg-user", default="root", help="Postgres username")
+@click.option("--pg-pass", default="root", help="Postgres password")
+@click.option("--pg-db", default="ny_taxi", help="Postgres database name")
+@click.option("--pg-host", default="localhost", help="Postgres host")
+@click.option("--pg-port", default=5432, type=int, help="Postgres port")
+@click.option("--chunksize", default=100000, type=int, help="CSV read chunksize")
+@click.option("--target-table", default="yellow_taxi_data", help="Target DB table name")
+def main(year, month, pg_user, pg_pass, pg_db, pg_host, pg_port, chunksize, target_table):
+    run(
+        year=year,
+        month=month,
+        pg_user=pg_user,
+        pg_pass=pg_pass,
+        pg_db=pg_db,
+        pg_host=pg_host,
+        pg_port=pg_port,
+        chunksize=chunksize,
+        target_table=target_table,
+    )
+
 
 if __name__ == '__main__':
-    run()
+    main()
